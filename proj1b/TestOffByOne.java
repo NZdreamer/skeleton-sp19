4f373e7d11f81c809462b679911fea83306260f2
NZdreamer <hi45288871@163.com>
2020-04-20 00:42:16 +0800

    complete proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -16,6 +16,7 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('A', 'B'));
         assertTrue(offByOne.equalChars('&', '%'));
         assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('a', 'A'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
     }
